4f23f447f7498a49cd7292567562fea02dcd0d75
Judit A <you@example.com>
2024-10-15 21:01:08 +0200

    parte de javascript terminada

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
 document.getElementById('btPreguntar').addEventListener('click', function() {
-    
-})
+    const pregunta = document.getElementById('pregunta').value;
+    const respuesta = [
+        "Yo digo no",
+        "Claro que sí reina",
+        "No lo veo claro, pregunta de nuevo...",
+        "Yo no contaría con ello...",
+        "Puede ser, el límite es tu imaginación...",
+        "Por supuesto que SÍ"
+    ];
+  if(pregunta.trim() !== '') {
+    const respuestaRandom = respuesta[Math.floor(Math.random() * respuesta.length)];
+    document.getElementById('respuesta').innerText = respuestaRandom;
+    document.getElementById('pregunta').value = '';
+  } else {
+    alert('Tienes que preguntar algo...');
+  }
+});
